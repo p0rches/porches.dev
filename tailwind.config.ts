06049a07aae9dcd2e06269709cb6a3d3cf708840
Alex <ascott.sco@gmail.com>
2023-07-28 09:49:44 +0100

    Updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,15 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "var(--primary-color)",
+        "secondary-color": "var(--secondary-color)",
+        "accent-color": "var(--accent-color)",
+        "background-color": "var(--background-color)",
+        "text-color": "var(--text-color)",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
